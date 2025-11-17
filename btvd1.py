a843193932fb8a8ed0bce731fc407c0e8e5b5262
nguyenthanhan27082005-boop <nguyenthanhan27082005@gmail.com>
2025-11-17 15:03:05 +0700

    Add files via upload

---
+++ btvd1.py
@@ -0,0 +1,23 @@
+# VD1
+a = 2
+while a <= 10:
+    print(f"{a}")
+    a +=2
+# VD2
+b = 3
+while b <= 18:
+    print(f"{b}")
+    b +=3
+# VD3
+c = 5
+while c <= 100:
+    print(f"{c}")
+    c +=5
+# VD4
+d = 1
+while d <= 100:
+    if d == 1:
+        print(f"{d}", end= " ")
+    else:
+        print(f"+{d}", end = " ")
+    d += 1
